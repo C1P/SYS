f6c27230531da75b9280f98e3c4d72548977d39a
c1p <chan_0426@hotmail.com>
2016-07-25 10:10:20 +0800

    SYS

---
+++ 优化后的进制转化.R
@@ -0,0 +1,7 @@
+##优化后的进制转化
+f = function(n, k = 2) {
+    if (k > 9) stop("")
+    if (n == 0) return("0")
+    if (n < 0) return(paste0("-", f(-n, k)))
+    do.call(paste0, as.list(floor(n / k^((ceiling(log(n, k)):1-1))) %% k))
+}
